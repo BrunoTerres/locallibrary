84e53ba72b37b7166ff7e733b951e18ddcec43ee
BrunoTerres <brunoeterres@gmail.com>
2021-01-06 17:13:19 -0300

    New model book 'Language' added to catalog app

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -17,6 +17,14 @@ class Genre(models.Model):
         return self.name
     
 
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200, help_text="Enter the book's natural language (e.g. English, French, Japanese, etc)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
+
 class Book(models.Model):
     """
     Model representing a book (but not a specific copy of a book).
@@ -98,4 +106,3 @@ class Author(models.Model):
         String for representing the Model object.
         """
         return f'{self.last_name}, {self.first_name}'
-        
